045e24608e80a0ddce42548b293d7c296bb60426
Gilbert <akhamgilbert@gmail.com>
2023-10-20 23:49:39 +0530

    some changes in jobs.vue

--- components/Jobs.vue
+++ components/Jobs.vue
@@ -23,9 +23,9 @@
                                 </p>
 
                                 <p class="text-gray-600 flex items-center mr-10">
-                                    Short by:
+                                    Status:
                                     <span class="font-bold text-sm text-black flex items-center ml-2">
-                                    Creation Date
+                                    All Status
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
   <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
 </svg>
